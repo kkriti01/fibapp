86f781413be93d52c2c093ad12216d5e810f2ada
kriti <kriti.cs10@gmai.com>
2016-08-07 18:51:26 +0530

    first commit

---
+++ fib/views.py
@@ -0,0 +1,32 @@
+import time
+from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class FibView(View):
+    def get(self, request, *args, **kwargs):
+        start = time.clock()
+        num = request.GET.get('number')
+        # validatiostrinh
+        if num.isdigit():
+            number = int(0 if num is None else num)
+            result = self.fibonacci(number)
+            end = time.clock()
+            elapsed = end - start
+            fib_result = {'number': number, 'result': result, 'elapsed': elapsed}
+            return render(request, 'fibonacci.html', {'fib_result': fib_result})
+        else:
+            return HttpResponse("please enter valid number")
+
+    def fibonacci(self, number):
+        a = 1
+        b = 1
+        c = a + b
+        count = 2
+        while number > count:
+            c = a + b
+            a = b
+            b = c
+            count = count + 1
+        return c
