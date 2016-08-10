cf4ecc2dfe138940cb9b37dcbd9274f776d7f4f4
kriti <kriti.cs10@gmai.com>
2016-08-10 21:45:38 +0530

    test cases added

--- fib/tests.py
+++ fib/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class TestFib(TestCase):
+    def test_fib_success_number(self):
+        c = Client()
+        response = c.get('/', {'number': 2})
+        self.assertEqual(response.status_code, 200)
+
+    def test_fib_success(self):
+        c = Client()
+        response = c.get('/', {'number': 'R'})
+        self.assertEqual(response.status_code, 200)
--- fib/views.py
+++ fib/views.py
@@ -1,5 +1,4 @@
 import time
-from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
 
